7e7cdbd2389b08dcf25430cf14425d25c1d55708
Tsung Hung <hungmasterj@gmail.com>
2014-10-15 22:29:26 -0700

    first commit

---
+++ scrapy_craigslist/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_craigslist project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_craigslist'
+
+SPIDER_MODULES = ['scrapy_craigslist.spiders']
+NEWSPIDER_MODULE = 'scrapy_craigslist.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_craigslist (+http://www.yourdomain.com)'
